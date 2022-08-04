8a7013df2a7eec054f18e9969656221f412e03a7
Horndt <mattias.horndt@mercedes-benz.com>
2022-08-04 12:53:35 +0200

    create db-migrate order-products

--- migrations/sqls/20220804104245-users-table-up.sql
+++ migrations/sqls/20220804104245-users-table-up.sql
@@ -1,1 +1,7 @@
-/* Replace with your SQL commands */
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR(30),
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    user_password VARCHAR(250)
+    );
